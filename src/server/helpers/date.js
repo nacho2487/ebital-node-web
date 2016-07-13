4e4eb98c8599a71487f0f8c1f38c8fa8610b2724
Ignacio Olaizola <nacho2487@gmail.com>
2016-07-13 19:44:50 -0300

    fix minor issues on date

--- src/server/helpers/date.js
+++ src/server/helpers/date.js
@@ -1,6 +1,6 @@
 exports.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 exports.years = function(){
-	var currentYear = new Date().getFullYear(), 
+	var currentYear = new Date().getFullYear() + 15,
 		years = [],
 		startYear = 1980;
 	while ( startYear <= currentYear ) {
